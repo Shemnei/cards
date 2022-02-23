aa48ee96c739faa3f8ad0e664dda42e003f24ba6
Jonas Grawe <Shemnei@users.noreply.github.com>
2022-02-24 00:15:21 +0100

    Added pub uses to lib

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,8 @@ pub mod card;
 pub mod deck;
 pub mod rank;
 pub mod suit;
+
+pub use crate::card::Card;
+pub use crate::deck::{ArrayDeck, Deck52, VecDeck};
+pub use crate::rank::Rank;
+pub use crate::suit::Suit;
